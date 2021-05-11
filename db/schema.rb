1c0efdf159924447d0d311091061dfc5313fc3de
jessicaUP <uphoff.jessica@gmail.com>
2021-05-11 16:40:34 -0400

    Fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_10_181627) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "first_last_name", null: false
     t.string "email", null: false
