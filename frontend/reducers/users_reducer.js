003c301af9dcc31f85264014a0adac17bc1cd73a
jessicaUP <uphoff.jessica@gmail.com>
2021-05-11 14:05:22 -0400

    Prepare for initial Heroku push

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -2,4 +2,18 @@ import * as APIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'; 
+
+
+const usersReducer = (state = {}, action) => {
+  Object.freeze(state);
+  
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
