53a3f642e3481fe42133b7488b20de91d54b8746
jessicaUP <uphoff.jessica@gmail.com>
2021-05-11 15:32:44 -0400

    make it better

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux';
 
-import users from './users_reducer';
+import entities from './entities_reducer';
+import errors from './errors_reducer';
+import session from './session_reducer';
 
 
 const rootReducer = combineReducers({
+  entities,
   session,
-  users
+  errors
 })
 
 export default rootReducer;
--- frontend/wearby_parker.jsx
+++ frontend/wearby_parker.jsx
@@ -4,20 +4,19 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root')
-  let store;
+  let preloadedState = undefined;
+  
   if (window.currentUser) {
-    const preloadedState = {
+    preloadedState = {
       session: { id: window.currentUser.id },
       entities: {
         users: { [window.currentUser.id]: window.currentUser }
       }
     };
-    store = configureStore(preloadedState);
-    delete window.currentUser;
-  } else {
-    store = configureStore();
-  };
+  } 
+  
+  const root = document.getElementById('root')
+  const store = configureStore(preloadedState);
 
   ReactDOM.render(<Root store={store} />, root);
 })
