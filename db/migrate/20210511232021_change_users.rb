f161f15bc30f43c435182f2de1dfd5a28933320d
jessicaUP <uphoff.jessica@gmail.com>
2021-05-11 20:09:55 -0400

    Migration bug

--- db/migrate/20210511232021_change_users.rb
+++ db/migrate/20210511232021_change_users.rb
@@ -1,7 +1,7 @@
 class ChangeUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :name
-    add_column :users, :name, :string
+    remove_column :users, :first_last_name
+    add_column :users, :name, :string, uniqueness: true
     add_index :users, :name
   end
 end
