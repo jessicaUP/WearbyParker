e4b20008dc7e091eac161cbc48c3e36d7d09dc76
jessicaUP <uphoff.jessica@gmail.com>
2021-05-13 10:04:26 -0400

    Auth

---
+++ frontend/components/session_form/demo_user_container 2.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// import DemoUser from './demo_user'
+// import { connect } from 'react-redux'
+// import { login } from '../../actions/session_actions'
+// import { withRouter } from 'react-router'
+
+
+
+// const mDTP = dispatch => ({
+//   login: user => dispatch(login(user))
+// })
+
+// export default DemoUserContainer = withRouter(connect(null, mDTP)(DemoUser))
