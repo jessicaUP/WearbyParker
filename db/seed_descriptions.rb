2c5e109aa08a9de3853091caff74815af12dcd66
jessicaUP <uphoff.jessica@gmail.com>
2021-05-14 18:44:48 -0400

    Product & User seeds

---
+++ db/seed_descriptions.rb
@@ -0,0 +1,85 @@
+
+
+ainsley = "Ainsley makes a statement through ample acetate, a pronounced 
+  keyhole bridge, and our signature Graduated Rivet design\nMade from 
+  hand-polished cellulose acetate\nAkulon-coated screws for durability\n\n
+  Demonstrated frame widths\nAinsley is available in a medium width\nIf your 
+  face is average in size, which is most common, this frame should fit you well. 
+  Unsure of your size? Select a few different options in a free Home Try-On."
+
+butler = "Butler is a studious shape that would look right at home in a private
+ library with lots of leather-bound tomes. Looks great in the outside world
+ too, though.\nMade from hand-polished cellulose acetate\nAkulon-coated screws
+ for durability\n\nDemonstrated frame widths\nButler is available in narrow 
+ and medium\nChoose a width that corresponds with your face width. If you’re
+ still unsure, select a few different sizes in a free Home Try-On."
+
+elina = "Elina has way more to offer than its rounded, easy-to-wear cat-eye
+ shape. We elevated the frame with Art Deco-inspired metal temples, sleek 
+ temple tips, and a Graduated Rivet design.\nMade from hand-polished cellulose 
+ acetate and stainless steel\nAkulon-coated screws for durability\nDemonstrated
+ frame widths\nElina is available in a medium width\nIf your face is average 
+ in size, which is most common, this frame should fit you well. Unsure of your 
+ size? Select a few different options in a free Home Try-On."
+
+esme = "A head turn here. A double take there. Esme commands just this sort of 
+  attention with its squarish cat-eyed lenses and overall impression of 
+  understated confidence.\nMade from hand-polished cellulose acetate\n
+  Akulon-coated screws for durability\n\nDemonstrated frame widths\nEsme is 
+  available in narrow and medium\nChoose a width that corresponds with your 
+  face width. If you’re still unsure, select a few different sizes in a 
+  free Home Try-On."
+
+gillian = "With Gillian, grand proportions (oversized round lenses, anyone?) 
+  meet thinner acetate for an airier, softer look. Balance is key, after all.\n
+  Made from hand-polished cellulose acetate\nAkulon-coated screws for durability\n
+  Demonstrated frame widths\nGillian is available in a medium width\nIf your face 
+  is average in size, which is most common, this frame should fit you well. 
+  Unsure of your size? Select a few different options in a free Home Try-On."
+
+halton = "Halton's quiet confidence comes from its distinguished features: 
+  a mid-century acetate silhouette (with an angular bottom!), Art Deco-inspired 
+  metal temples, sleek temple tips, and a Graduated Rivet design.\nMade from 
+  hand-polished cellulose acetate and stainless steel\nAkulon-coated screws for 
+  durability\n\nDemonstrated frame widths\nHalton is available in a medium width\n
+  If your face is average in size, which is most common, this frame should fit 
+  you well. Unsure of your size? Select a few different options in a free Home Try-On."
+
+ivring = "Meticulously sculptured acetate surrounds Irving’s statement round 
+  lenses, and adds an element of depth and dimension to its smart-looking 
+  silhouette.\nMade from hand-polished cellulose acetate\nAkulon-coated screws 
+  for durability\n\nDemonstrated frame widths\nIrving is available in a wide width\n
+  If you have a wide or broad face, this frame should fit you well. Unsure of 
+  your size? Select a few different options in a free Home Try-On."
+
+jayla = "Jayla draws inspiration from a few of our favorite design eras, with 
+  its roomy mid-century acetate silhouette, Art Deco-inspired metal temples, 
+  sleek temple tips, and Graduated Rivet design.\nMade from hand-polished 
+  cellulose acetate and stainless steel\nAkulon-coated screws for durability\n
+  Demonstrated frame widths\n\nJayla is available in a wide width\nIf you have a 
+  wide or broad face, this frame should fit you well. Unsure of your size? Select 
+  a few different options in a free Home Try-On."
+
+newcomb = "A featherlight titanium style, Newcomb combines rounded lenses, a 
+  sleek brow bar, and custom tortoise patterns painted by hand and with heat.\n
+  Made from the strongest, lightest ion-plated titanium\nAkulon-coated screws 
+  for durability\nTitanium temple tips\n\nPVC nose pads prevent slipping and 
+  pressure\nDemonstrated frame widths\nNewcomb is available in a narrow width\n
+  If you have a slender or narrow face, this frame should fit you well. 
+  Unsure of your size? Select a few different options in a free Home Try-On."
+
+percey = "With round lenses and a keyhole bridge, Percey is a bookish modern 
+  classic.\nMade from hand-polished cellulose acetate\nAkulon-coated screws for 
+  durability\n\nDemonstrated frame widths\nFrom top to bottom: extra wide, wide, 
+  medium, extra narrow\nPercey is available in extra narrow, narrow, medium, 
+  wide, and extra wide\nChoose a width that corresponds with your own face width, 
+  e.g., pick a narrow frame if you have a narrow face (and so on). If you’re 
+  still unsure of your size, select a few different options in a free Home Try-On."
+
+thorpe = "Inspired by classic circular frames from various eras, Thorpe 
+  features Art Deco-inspired metal temples, sleek temple tips, and a Graduated 
+  Rivet design. If you're a fan of round lenses, look no further.\nMade from 
+  hand-polished cellulose acetate and stainless steel\nAkulon-coated screws for 
+  durability\n\nDemonstrated frame widths\nThorpe is available in a medium width\n
+  If your face is average in size, which is most common, this frame should fit 
+  you well. Unsure of your size? Select a few different options in a free Home Try-On."
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,112 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#DB CLEAR
+User.destroy_all
+Product.destroy_all
+Gender.destroy_all
+FrameWidth.destroy_all
+ProductsFrameWidth.destroy_all
+Shape.destroy_all
+ProductsShape.destroy_all
+Color.destroy_all
+ProductsColor.destroy_all
+Material.destroy_all
+ProductsMaterial.destroy_all
+NoseBridge.destroy_all
+ProductsNoseBridge.destroy_all
+
+#USERS --> address:first users:last
+jessica = User.create!(name: 'Jessica Uphoff', password: 'killercat', email: 'jessica@uphoff.com')
+nick = User.create!(name: 'Nick Missel', password: 'killercat', email: 'nick@missel.com')
+morgan = User.create!(name: 'Morgan Fritz', password: 'killercat', email: 'morgan@fritz.com')
+gabby = User.create!(name: 'Gabby West', password: 'killercat', email: 'gabby@west.com')
+karl = User.create!(name: 'Karl Smith', password: 'killercat', email: 'karl@smith.com')
+brad = User.create!(name: 'Brad Pitt', password: 'killercat', email: 'brad@pitt.com')
+bill = User.create!(name: 'Bill Skarsgard', password: 'killercat', email: 'bill@skarsgard.com')
+sam = User.create!(name: 'Samantha Frank', password: 'killercat', email: 'samantha@frank.com')
+claire = User.create!(name: 'Claire Rose', password: 'killercat', email: 'claire@rose.com')
+billy = User.create!(name: 'Billy Bob', password: 'killercat', email: 'billy@bob.com')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#PRODUCTS --> products:last
+
+w1_ainsley = Product.create!(gender_id: 1, name: 'Ainsley', color_name: 'Jet Black', details: ainsley, price: 95)
+m1_ainsley = Product.create!(gender_id: 2, name: 'Ainsley', color_name: 'Jet Black', details: ainsley, price: 95)
+w2_ainsley = Product.create!(gender_id: 1, name: 'Ainsley', color_name: 'Striped Cypress', details: ainsley, price: 95)
+m2_ainsley = Product.create!(gender_id: 2, name: 'Ainsley', color_name: 'Striped Cypress', details: ainsley, price: 95)
+
+w1_butler = Product.create!(gender_id: 1, name: 'Butler', color_name: 'Teal Tortoise', details: butler, price: 95)
+m1_butler = Product.create!(gender_id: 2, name: 'Butler', color_name: 'Teal Tortoise', details: butler, price: 95)
+w2_butler = Product.create!(gender_id: 1, name: 'Butler', color_name: 'Butterscotch Tortoise', details: butler, price: 95)
+m2_butler = Product.create!(gender_id: 2, name: 'Butler', color_name: 'Butterscotch Tortoise', details: butler, price: 95)
+w3_butler = Product.create!(gender_id: 1, name: 'Butler', color_name: 'Shoreline Fade', details: butler, price: 95)
+m3_butler = Product.create!(gender_id: 2, name: 'Butler', color_name: 'Shoreline Fade', details: butler, price: 95)
+w4_butler = Product.create!(gender_id: 1, name: 'Butler', color_name: 'Jet Black', details: butler, price: 95)
+m4_butler = Product.create!(gender_id: 2, name: 'Butler', color_name: 'Jet Black', details: butler, price: 95)
+
+w1_elina = Product.create!(gender_id: 1, name: 'Elina', color_name: 'Truffle Tortoise with Polished Gold', details: elina, price: 145)
+w2_elina = Product.create!(gender_id: 1, name: 'Elina', color_name: 'Blue Agave with Polished Gold', details: elina, price: 145)
+
+w1_esme = Product.create!(gender_id: 1, name: 'Esme', color_name: 'Sesame Tortoise', details: esme, price: )
+m1_esme = Product.create!(gender_id: 2, name: 'Esme', color_name: 'Sesame Tortoise', details: esme, price: )
+w2_esme = Product.create!(gender_id: 1, name: 'Esme', color_name: 'Blue Thistle', details: esme, price: )
+m2_esme = Product.create!(gender_id: 2, name: 'Esme', color_name: 'Blue Thistle', details: esme, price: )
+w3_esme = Product.create!(gender_id: 1, name: 'Esme', color_name: 'Oxblood', details: esme, price: )
+m3_esme = Product.create!(gender_id: 2, name: 'Esme', color_name: 'Oxblood', details: esme, price: )
+
+w1_gillian = Product.create!(gender_id: 1, name: 'Gillian', color_name: 'Teal Tortoise', details: gillian, price: 95)
+m1_gillian = Product.create!(gender_id: 2, name: 'Gillian', color_name: 'Teal Tortoise', details: gillian, price: 95)
+w2_gillian = Product.create!(gender_id: 1, name: 'Gillian', color_name: 'Jet Black', details: gillian, price: 95)
+m2_gillian = Product.create!(gender_id: 2, name: 'Gillian', color_name: 'Jet Black', details: gillian, price: 95)
+w3_gillian = Product.create!(gender_id: 1, name: 'Gillian', color_name: 'Nutmeg Crystal', details: gillian, price: 95)
+m3_gillian = Product.create!(gender_id: 2, name: 'Gillian', color_name: 'Nutmeg Crystal', details: gillian, price: 95)
+
+w_halton = Product.create!(gender_id: 1, name: 'Halton', color_name: 'Jet Black with Polished Gold', details: halton, price: 145)
+m_halton = Product.create!(gender_id: 2, name: 'Halton', color_name: 'Jet Black with Polished Gold', details: halton, price: 145)
+w_halton = Product.create!(gender_id: 1, name: 'Halton', color_name: 'Nutmeg Crystal with Riesling', details: halton, price: 145)
+m_halton = Product.create!(gender_id: 2, name: 'Halton', color_name: 'Nutmeg Crystal with Riesling', details: halton, price: 145)
+
+w1_ivring = Product.create!(gender_id: 1, name: 'Ivring', color_name: 'Black Oak Tortoise', details: ivring, price: 95)
+m1_ivring = Product.create!(gender_id: 2, name: 'Ivring', color_name: 'Black Oak Tortoise', details: ivring, price: 95)
+w2_ivring = Product.create!(gender_id: 1, name: 'Ivring', color_name: 'Pacific Crystal', details: ivring, price: 95)
+m2_ivring = Product.create!(gender_id: 2, name: 'Ivring', color_name: 'Pacific Crystal', details: ivring, price: 95)
+
+w1_jayla = Product.create!(gender_id: 1, name: 'Jayla', color_name: 'Russet Red with Riesling', details: jayla, price: 145)
+m1_jayla = Product.create!(gender_id: 2, name: 'Jayla', color_name: 'Russet Red with Riesling', details: jayla, price: 145)
+w2_jayla = Product.create!(gender_id: 1, name: 'Jayla', color_name: 'Cognac Tortoise with Polished Gold', details: jayla, price: 145)
+m2_jayla = Product.create!(gender_id: 2, name: 'Jayla', color_name: 'Cognac Tortoise with Polished Gold', details: jayla, price: 145)
+
+w1_newcomb = Product.create!(gender_id: 1, name: 'Newcomb', color_name: 'Polished Gold with Savanna Tortoise', details: newcomb, price: 195)
+m1_newcomb = Product.create!(gender_id: 2, name: 'Newcomb', color_name: 'Polished Gold with Savanna Tortoise', details: newcomb, price: 195)
+
+w1_percey = Product.create!(gender_id: 1, name: 'Percey', color_name: 'Crystal', details: percey, price: 95)
+m1_percey = Product.create!(gender_id: 2, name: 'Percey', color_name: 'Crystal', details: percey, price: 95)
+w2_percey = Product.create!(gender_id: 1, name: 'Percey', color_name: 'Petal Tortoise', details: percey, price: 95)
+m2_percey = Product.create!(gender_id: 2, name: 'Percey', color_name: 'Petal Tortoise', details: percey, price: 95)
+w3_percey = Product.create!(gender_id: 1, name: 'Percey', color_name: 'Tidal Blue', details: percey, price: 95)
+m3_percey = Product.create!(gender_id: 2, name: 'Percey', color_name: 'Tidal Blue', details: percey, price: 95)
+w4_percey = Product.create!(gender_id: 1, name: 'Percey', color_name: 'Striped Sassafras', details: percey, price: 95)
+m4_percey = Product.create!(gender_id: 2, name: 'Percey', color_name: 'Striped Sassafras', details: percey, price: 95)
+w5_percey = Product.create!(gender_id: 1, name: 'Percey', color_name: 'Crystal with Oak Barrel and Blue Bay', details: percey, price: 95)
+m5_percey = Product.create!(gender_id: 2, name: 'Percey', color_name: 'Crystal with Oak Barrel and Blue Bay', details: percey, price: 95)
+
+w5_thorpe = Product.create!(gender_id: 1, name: 'Thorpe', color_name: 'Poblano with Polished Silver', details: thorpe, price: 95)
+m5_thorpe = Product.create!(gender_id: 2, name: 'Thorpe', color_name: 'Poblano with Polished Silver', details: thorpe, price: 95)
+w5_thorpe = Product.create!(gender_id: 1, name: 'Thorpe', color_name: 'Acorn Tortoise with Riesling', details: thorpe, price: 95)
+m5_thorpe = Product.create!(gender_id: 2, name: 'Thorpe', color_name: 'Acorn Tortoise with Riesling', details: thorpe, price: 95)
+
+
