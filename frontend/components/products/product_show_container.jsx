27cad5f783f0826b91bdea191f4f125a7f6ff0ca
jessicaUP <uphoff.jessica@gmail.com>
2021-06-30 23:46:12 -0400

    Clean up code

--- frontend/components/products/product_show_container.jsx
+++ frontend/components/products/product_show_container.jsx
@@ -1,18 +1,18 @@
 import { connect } from 'react-redux';
-import { fetchProduct } from '../../actions/product_actions'
-import { createCartItem } from '../../actions/cart_item_actions'
-import ProductShow from './product_show'
+import { fetchProduct } from '../../actions/product_actions';
+import { createCartItem } from '../../actions/cart_item_actions';
+import ProductShow from './product_show';
 import { withRouter } from 'react-router-dom';
 
 const mSTP = (state, ownProps) => {
   return ({
     product: state.entities.products[ownProps.match.params.productId]
   })
-}
+};
 
 const mDTP = dispatch => ({
   fetchProduct: productId => dispatch(fetchProduct(productId)),
   createCartItem: cartItem => dispatch(createCartItem(cartItem))
-})
+});
 
-export default withRouter(connect(mSTP, mDTP)(ProductShow))
+export default withRouter(connect(mSTP, mDTP)(ProductShow));
