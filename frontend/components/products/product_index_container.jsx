da722faf5b3f0d19e98f8f7bb8e28f781c75acc0
jessicaUP <uphoff.jessica@gmail.com>
2021-06-30 23:45:04 -0400

    Clean up code

--- frontend/components/products/product_index_container.jsx
+++ frontend/components/products/product_index_container.jsx
@@ -1,18 +1,18 @@
 import { connect } from 'react-redux';
-import { fetchGenderProducts } from '../../actions/gender_actions'
-import ProductIndex from './product_index'
+import { fetchGenderProducts } from '../../actions/gender_actions';
+import ProductIndex from './product_index';
 import { withRouter } from 'react-router-dom';
 
 const mSTP = (state, ownProps) => {
   return ({
     genderId: state.entities.genderProducts[ownProps.match.params.genderId],
   })
-}
+};
 
 const mDTP = dispatch => {
   return ({
     fetchGenderProducts: genderId => dispatch(fetchGenderProducts(genderId))
   })
-}
+};
 
-export default withRouter(connect(mSTP, mDTP)(ProductIndex))
+export default withRouter(connect(mSTP, mDTP)(ProductIndex));
