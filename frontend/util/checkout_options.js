51cab412fad5415fa267347a4850201b94b4b027
jessicaUP <uphoff.jessica@gmail.com>
2021-07-23 15:21:50 -0400

    Dry add item form"

---
+++ frontend/util/checkout_options.js
@@ -0,0 +1,75 @@
+
+
+export const prescriptionOptions = [
+  {
+    type: 'prescription_type',
+    name: 'Single-vision',
+    price: 95,
+    priceName: '$95',
+    desc: 'Corrects one field of vision(near, intermediate, or distance)'
+  },
+  {
+    type: 'prescription_type',
+    name: 'Progressives',
+    priceName: '$295',
+    price: 295,
+    desc: 'Correct near, intermediate, and distance fields of vision in one lens so you don\'t have to switch between multiple pairs'
+  },
+  {
+    type: 'prescription_type',
+    name: 'Non-prescription',
+    priceName: '$95',
+    price: 95,
+    desc: 'Offers style and protection with no vision correction'
+  },
+  {
+    type: 'prescription_type',
+    name: 'Readers',
+    priceName: '$95',
+    price: 95,
+    desc: 'Offers simple magnification for, well, reading (no prescription necessary)'
+  }
+];
+
+
+export const lenseOptions = [
+  {
+    type: 'lense_type',
+    name: 'Classic',
+    priceName: 'Free',
+    price: 0,
+    desc: 'Scratch-resistant, anti-reflective lenses that block 100% of UV rays'
+  },
+  {
+    type: 'lense_type',
+    name: 'Blue-light-filtering',
+    priceName: '$50',
+    price: 50,
+    desc: 'Scratch-resistant, anti-reflective lenses that block 100% of UV rays; they also filter more blue light from digital screens and the sun then our classic or light-responsive lenses.'
+  },
+  {
+    type: 'lense_type',
+    name: 'Light-responsive',
+    priceName: '$100',
+    price: 100,
+    desc: 'Scratch-resistant, anti-reflective lenses that block 100% of UV rays and the transition from clear to a darker tint outdoors'
+  }
+];
+
+
+export const materialOptions = [
+  {
+    type: 'lense_material',
+    name: 'Polycerbonate',
+    priceName: 'Free',
+    price: 0,
+    desc: 'Scratch-resistant, anti-reflective lenses that block 100% of UV rays'
+  },
+  {
+    type: 'lense_material',
+    name: '1.67 high-index',
+    priceName: '$30',
+    price: 30,
+    desc: 'Scratch-resistant, anti-reflective lenses that block 100% of UV rays; they also filter more blue light from digital screens and the sun then our classic or light-responsive lenses.'
+  }
+];
